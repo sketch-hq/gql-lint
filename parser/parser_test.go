89bc2c6cc56bb835f0333862fb7a1ee803373e88
uesteibar <uesteibar@gmail.com>
2023-01-13 10:16:45 +0100

    Add test for ParseQueryDir

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,7 +11,7 @@ func TestParseSchemaFile(t *testing.T) {
 		t.Fatalf("Unexpected error %s", err)
 	}
 
-	deprecatedField := schema.Types["Book"].Fields[0]
+	deprecatedField := schema.Types["Book"].Fields[1]
 
 	if deprecatedField.Name != "title" {
 		t.Fatalf("expected field '%s' to be 'title", deprecatedField.Name)
@@ -35,7 +35,7 @@ func TestParseSchemaFile(t *testing.T) {
 		t.Fatalf("%s expected to be deprecated", deprecatedField.Name)
 	}
 
-	field := schema.Types["Book"].Fields[1]
+	field := schema.Types["Book"].Fields[2]
 
 	if field.Name != "author" {
 		t.Fatalf("expected field '%s' to be 'title", field.Name)
@@ -58,5 +58,32 @@ func TestParseSchemaFile_NotFound(t *testing.T) {
 
 	if err == nil {
 		t.Fatalf("Expected error, got none")
+	}
+}
+
+func TestParseQueryDir(t *testing.T) {
+	schema, err := ParseSchemaFile("./fixtures/with_deprecations.gql")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	fields, err := ParseQueryDir("./fixtures/queries", schema)
+
+	if len(fields) != 1 {
+		t.Fatalf("expected only one deprecation, got %d", len(fields))
+	}
+
+	field := fields[0]
+
+	if field.Path == "author.book.title" {
+		t.Fatalf("expected 'author.book.title', got %s", field.Path)
+	}
+
+	if !field.IsDeprecated {
+		t.Fatalf("expected '%s' to be deprecated", field.Path)
+	}
+
+	if field.File != "fixtures/queries/deprecation.gql" || field.Line != 7 {
+		t.Fatalf("unexpected location for deprecation warning: %s:%d", field.File, field.Line)
 	}
 }
