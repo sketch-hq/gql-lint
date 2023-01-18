1979a3a1d603c960cce4d9ca3ce6fa0a3cdbe705
uesteibar <uesteibar@gmail.com>
2023-01-18 11:12:33 +0100

    Make GetUnusedFields support multiple query sets

--- unused/unused_test.go
+++ unused/unused_test.go
@@ -4,35 +4,20 @@ import (
 	"testing"
 
 	"github.com/matryer/is"
-	"github.com/sketch-hq/gql-lint/parser"
 )
 
 func TestGetUnusedFields(t *testing.T) {
 	is := is.New(t)
 
-	unusedFields, err := GetUnusedFields("testdata/schemas/with_deprecations.gql", "testdata/queries/deprecation.gql")
+	unusedFields, err := GetUnusedFields(
+		"testdata/schemas/with_deprecations.gql",
+		[]string{
+			"testdata/queries/one.gql",
+			"testdata/queries/deprecation.gql",
+		},
+	)
 	is.NoErr(err)
 
 	is.Equal(len(unusedFields), 1)
-
 	is.Equal(unusedFields[0].Name, "Book.oldTitle")
-}
-
-func TestIsFieldUsed(t *testing.T) {
-	is := is.New(t)
-
-	schema, err := parser.ParseSchemaFile("testdata/schemas/with_deprecations.gql")
-	is.NoErr(err)
-
-	query, err := parser.ParseQueryDir("testdata/queries/deprecation.gql", schema)
-	is.NoErr(err)
-
-	used := IsFieldUsed(parser.SchemaField{Name: "Book.title"}, query)
-	is.True(used)
-
-	query, err = parser.ParseQueryDir("testdata/queries/one.gql", schema)
-	is.NoErr(err)
-
-	used = IsFieldUsed(parser.SchemaField{Name: "Book.title"}, query)
-	is.True(!used)
 }
