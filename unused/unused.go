69d9167798a616f2c34b7230f13d4c41941cca86
Nestor Lafon Gracia <nestor@sketch.com>
2023-01-18 12:03:29 +0100

    merge main

--- unused/unused.go
+++ unused/unused.go
@@ -2,35 +2,63 @@ package unused
 
 import "github.com/sketch-hq/gql-lint/parser"
 
-func GetUnusedFields(schemaPath string, queriesPath string) ([]parser.SchemaField, error) {
-	var unusedFields []parser.SchemaField
+type UnusedField struct {
+	Name string
+}
+
+type unusedRegistry map[string]UnusedField
+
+func (r unusedRegistry) Record(field parser.SchemaField) {
+	r[field.Name] = UnusedField{Name: field.Name}
+}
+
+func GetUnusedFields(schemaPath string, queriesPaths []string) ([]UnusedField, error) {
+	unusedFields := make(unusedRegistry)
 
 	schema, err := parser.ParseSchemaFile(schemaPath)
 	if err != nil {
-		return unusedFields, err
+		return []UnusedField{}, err
 	}
 
 	deprecatedFields := parser.ParseDeprecatedFields(schema)
 
-	queries, err := parser.ParseQuerySource(queriesPath, schema)
-	if err != nil {
-		return unusedFields, err
-	}
+	for _, queriesPath := range queriesPaths {
+		queries, err := parser.ParseQuerySource(queriesPath, schema)
+		if err != nil {
+			return []UnusedField{}, err
+		}
 
-	for _, deprecatedField := range deprecatedFields {
-		if !IsFieldUsed(deprecatedField, queries) {
-			unusedFields = append(unusedFields, deprecatedField)
+		for _, deprecatedField := range deprecatedFields {
+			used := isUsed(deprecatedField, queries)
+			recorded := isRecorded(deprecatedField, unusedFields)
+
+			if used && recorded {
+				delete(unusedFields, deprecatedField.Name)
+			} else if !used && !recorded {
+				unusedFields.Record(deprecatedField)
+			}
 		}
 	}
 
-	return unusedFields, nil
+	result := make([]UnusedField, 0, len(unusedFields))
+	for _, field := range unusedFields {
+		result = append(result, field)
+	}
+
+	return result, nil
 }
 
-func IsFieldUsed(field parser.SchemaField, queryFields parser.QueryFieldList) bool {
+func isUsed(field parser.SchemaField, queryFields parser.QueryFieldList) bool {
 	for _, queryField := range queryFields {
 		if queryField.SchemaPath == field.Name {
 			return true
 		}
 	}
 	return false
+}
+
+func isRecorded(field parser.SchemaField, registry unusedRegistry) bool {
+	_, found := registry[field.Name]
+
+	return found
 }
--- unused/unused_test.go
+++ unused/unused_test.go
@@ -4,35 +4,20 @@ import (
 	"testing"
 
 	"github.com/matryer/is"
-	"github.com/sketch-hq/gql-lint/parser"
 )
 
 func TestGetUnusedFields(t *testing.T) {
 	is := is.New(t)
 
-	unusedFields, err := GetUnusedFields("testdata/schemas/with_deprecations.gql", "testdata/queries/deprecation")
+	unusedFields, err := GetUnusedFields(
+		"testdata/schemas/with_deprecations.gql",
+		[]string{
+			"testdata/queries/one",
+			"testdata/queries/deprecation",
+		},
+	)
 	is.NoErr(err)
 
 	is.Equal(len(unusedFields), 1)
-
 	is.Equal(unusedFields[0].Name, "Book.oldTitle")
-}
-
-func TestIsFieldUsed(t *testing.T) {
-	is := is.New(t)
-
-	schema, err := parser.ParseSchemaFile("testdata/schemas/with_deprecations.gql")
-	is.NoErr(err)
-
-	query, err := parser.ParseQuerySource("testdata/queries/deprecation", schema)
-	is.NoErr(err)
-
-	used := IsFieldUsed(parser.SchemaField{Name: "Book.title"}, query)
-	is.True(used)
-
-	query, err = parser.ParseQuerySource("testdata/queries/one", schema)
-	is.NoErr(err)
-
-	used = IsFieldUsed(parser.SchemaField{Name: "Book.title"}, query)
-	is.True(!used)
 }
