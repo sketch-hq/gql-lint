a1fe7e16a56929632bb740b1100f37b7b02c61b7
Unai Esteibar <uesteibar@gmail.com>
2023-01-31 15:25:26 +0100

    Sort results from `unused` alphabetically (#21)

--- unused/unused_test.go
+++ unused/unused_test.go
@@ -13,12 +13,28 @@ func TestGetUnusedFields(t *testing.T) {
 	schema, err := sources.LoadSchema("testdata/schemas/with_deprecations.gql")
 	is.NoErr(err)
 
-	unusedFields, err := GetUnusedFields(schema, []string{
-		"testdata/queries/one/one.gql",
-		"testdata/queries/deprecation/deprecation.gql",
-	})
-	is.NoErr(err)
+	t.Run("reports unused fields", func(t *testing.T) {
+		is := is.New(t)
+		unusedFields, err := GetUnusedFields(schema, []string{
+			"testdata/queries/one/one.gql",
+			"testdata/queries/deprecation/deprecation.gql",
+		})
+		is.NoErr(err)
 
-	is.Equal(len(unusedFields), 1)
-	is.Equal(unusedFields[0].Name, "Book.oldTitle")
+		is.Equal(len(unusedFields), 1)
+		is.Equal(unusedFields[0].Name, "Book.oldTitle")
+	})
+
+	t.Run("sorts fields aphabetically", func(t *testing.T) {
+		is := is.New(t)
+		unusedFields, err := GetUnusedFields(schema, []string{
+			"testdata/queries/one/one.gql",
+		})
+		is.NoErr(err)
+
+		is.Equal(len(unusedFields), 2)
+
+		is.Equal(unusedFields[0].Name, "Book.oldTitle")
+		is.Equal(unusedFields[1].Name, "Book.title")
+	})
 }
