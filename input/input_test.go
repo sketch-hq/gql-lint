69ebb4d467d74e676d68768fe94246783a9b32c3
Ole Morten Halvorsen <ole@sketch.com>
2023-02-14 10:56:41 +0100

    Support for `--include` that works the opposite as `--ignore`
    Useful if you need to process a directory that contains a lot of other things.
    Instead of ignoreing stuff you can now filter out just the files you need.
    
    This also changes `--schema` and `--ignore` to support comma seperated arguments
    instead of the argument having to be repeated. This helps cut down the amount of
    processing needed to generate the argument list in some cases.

--- input/input_test.go
+++ input/input_test.go
@@ -10,10 +10,11 @@ func TestQueryFiles(t *testing.T) {
 	is := is.New(t)
 
 	tests := []struct {
-		name   string
-		args   []string
-		ignore []string
-		want   []string
+		name    string
+		args    []string
+		ignore  []string
+		include []string
+		want    []string
 	}{
 		{
 			name: "expands directories",
@@ -47,11 +48,23 @@ func TestQueryFiles(t *testing.T) {
 			ignore: []string{"./testdata/**/one.gql"},
 			want:   []string{"testdata/two.gql", "testdata/nested/two.gql"},
 		},
+		{
+			name:    "removes everything not matching an include filter",
+			args:    []string{"testdata/**/*.gql"},
+			include: []string{"testdata/one.gql"},
+			want:    []string{"testdata/one.gql"},
+		},
+		{
+			name:    "removes everything not matching a nested directory include filter",
+			args:    []string{"testdata/**/*.gql"},
+			include: []string{"./**/one.gql"},
+			want:    []string{"testdata/one.gql", "testdata/nested/one.gql"},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ExpandGlobs(tt.args, tt.ignore)
+			got, err := ExpandGlobs(tt.args, tt.include, tt.ignore)
 			is.NoErr(err)
 
 			is.Equal(got, tt.want)
